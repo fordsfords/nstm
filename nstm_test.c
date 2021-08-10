79d1453ed0df0efdd11be4877cdd459729d167ca
fordsfords <sford@geeky-boy.com>
2021-08-10 17:02:57 -0400

    More stats

--- nstm_test.c
+++ nstm_test.c
@@ -43,6 +43,7 @@ int main(int argc, char **argv)
   uint64_t t1, t2, t3, t4, t5, t6, t7, t_ignore;
   uint64_t t1_raw, t2_raw, t3_raw, t4_raw, t5_raw, t6_raw, t7_raw;
   uint64_t t1_rt, t2_rt, t3_rt, t4_rt, t5_rt, t6_rt, t7_rt;
+  uint64_t max_histo_diff, min_histo_diff;
 
   nstm = nstm_create(NSTM_CLOCK_MONOTONIC);
   nstm_raw = nstm_create(NSTM_CLOCK_MONOTONIC_RAW);
@@ -116,6 +117,75 @@ int main(int argc, char **argv)
       M, t6-t5, t6_raw-t5_raw, t6_rt-t5_rt);
   printf("%dM nstm_get best calls=%"PRIu64", raw=%"PRIu64", rt=%"PRIu64"\n",
       M, t7-t6, t7_raw-t6_raw, t7_rt-t6_rt);
+
+  /* Generate histogram of execution times. */
+  {
+    #define HISTO_SZ 10000
+    int histo[HISTO_SZ];
+    int i;
+    for (i=0; i<HISTO_SZ; i++) histo[i] = 0;
+
+    max_histo_diff = 0;
+    min_histo_diff = -1;
+    for (i=0; i<1000000; i++) {
+      uint64_t t1;
+      uint64_t t3;
+      uint64_t diff;
+      t1 = nstm_get(nstm_best);
+      (void)nstm_get(nstm_best);
+      t3 = nstm_get(nstm_best);
+      diff = t3-t1;
+      if (diff > max_histo_diff) {
+        max_histo_diff = diff;
+      }
+      if (diff < min_histo_diff) {
+        min_histo_diff = diff;
+      }
+      diff = diff / UINT64_C(10);
+      if (diff >= HISTO_SZ) {
+        diff = (HISTO_SZ - 1);
+      }
+      histo[diff]++;
+    }
+
+    for (i=0; i<HISTO_SZ; i++) {
+      if (histo[i] > 0) {
+        printf("histo[%d]=%d\n", i, histo[i]);
+      }
+    }
+    printf("max_histo_diff=%"PRIu64", min_histo_diff=%"PRIu64"\n",
+        max_histo_diff, min_histo_diff);
+  }
+
+  /* Get a series of calls. */
+  {
+    #define SERIES_SZ 100
+    uint64_t series[SERIES_SZ];
+    int i;
+    uint64_t max_series_diff;
+
+    /* Warm-up run. */
+    for (i = 0; i < SERIES_SZ; i++) {
+      series[i] = nstm_get(nstm_best);
+    }
+
+    /* Look for a run with pretty big outlier. */
+    max_series_diff = 0;
+    while (max_series_diff < (max_histo_diff / 3)) {
+      for (i = 0; i < SERIES_SZ; i++) {
+        series[i] = nstm_get(nstm_best);
+      }
+      max_series_diff = 0;
+      for (i = 1; i < SERIES_SZ; i++) {
+        if ((series[i] - series[i-1]) > max_series_diff) {
+          max_series_diff = series[i] - series[i-1];
+        }
+      }
+    }  /* while max_series_diff */
+    for (i = 1; i < SERIES_SZ; i++) {
+      printf("series diff %d=%"PRIu64"\n", i, series[i] - series[i-1]);
+    }
+  }
 
   nstm_delete(nstm);
   nstm_delete(nstm_raw);
--- tst.sh
+++ tst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # tst.sh - script to build nstm and its test program.
 
-gcc -o nstm_test nstm.c nstm_test.c
+gcc -Wall -o nstm_test nstm.c nstm_test.c
 if [ $? -ne 0 ]; then exit 1; fi
 
 ./nstm_test
